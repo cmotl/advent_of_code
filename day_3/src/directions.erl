b4625b27287907f9bbc0a88af51307d8c83fcc84
Christopher Motl <cmotl@cmotl.com>
2015-12-19 20:57:12 -0600

    Added transition function to find next house from current house given a direction.
    Better names could be something along the lines of navigate, navigation, compass, gps

--- day_3/src/directions.erl
+++ day_3/src/directions.erl
@@ -1,9 +1,12 @@
 -module(directions).
--export([parse_direction/1]).
+-export([parse_direction/1, follow_direction_from_house/2]).
 
 parse_direction($^) -> north;
 parse_direction($v) -> south;
 parse_direction($>) -> east;
 parse_direction($<) -> west.
 
-
+follow_direction_from_house({house, {X,Y}},north) -> {house, {X,Y+1}};
+follow_direction_from_house({house, {X,Y}},south) -> {house, {X,Y-1}};
+follow_direction_from_house({house, {X,Y}},east) -> {house, {X+1,Y}};
+follow_direction_from_house({house, {X,Y}},west) -> {house, {X-1,Y}}.
--- day_3/test/directions_tests.erl
+++ day_3/test/directions_tests.erl
@@ -6,3 +6,12 @@ parse_direction_test() ->
     ?assertEqual(south, directions:parse_direction($v)),
     ?assertEqual(east, directions:parse_direction($>)),
     ?assertEqual(west, directions:parse_direction($<)).
+
+move_from_house_test() -> 
+    ?assertEqual({house, {0,1}}, directions:follow_direction_from_house({house, {0,0}},north)),
+    ?assertEqual({house, {0,-1}}, directions:follow_direction_from_house({house, {0,0}},south)),
+    ?assertEqual({house, {1,0}}, directions:follow_direction_from_house({house, {0,0}},east)),
+    ?assertEqual({house, {-1,0}}, directions:follow_direction_from_house({house, {0,0}},west)).
+
+    
+
