8857cce4c0d0674ce9efbd8f9e21bc7a11963e60
Christopher Motl <cmotl@cmotl.com>
2015-12-21 20:04:55 -0600

    More of Day 3. Full parsing of input and matrics

--- day_3/src/directions.erl
+++ day_3/src/directions.erl
@@ -1,12 +1,72 @@
 -module(directions).
--export([parse_direction/1, follow_direction_from_house/2]).
+-export([parse_direction/1, follow_direction_from_house/2, directions_to_addresses/1, number_of_houses_to_visit/1, radio_directions_from_file/1, cardinal_directions_from_radio_directions/1, houses_to_visit/1, divide_directions/1, number_of_houses_to_visit_with_help/1, houses_to_visit_with_help/1]).
 
 parse_direction($^) -> north;
 parse_direction($v) -> south;
 parse_direction($>) -> east;
 parse_direction($<) -> west.
 
+radio_directions_from_file(Filename) ->
+    [_|Lists_of_directions] = chars:for_each_char_in_file(Filename, fun(H, Tail) -> [H|Tail] end, [read], []),
+    Directions_to_flatten = lists:reverse(Lists_of_directions),
+    lists:flatten(Directions_to_flatten).
+
+
+cardinal_directions_from_radio_directions(Radio_directions) ->
+    lists:map(fun(X) -> parse_direction(X) end, Radio_directions).
+
 follow_direction_from_house({house, {X,Y}},north) -> {house, {X,Y+1}};
 follow_direction_from_house({house, {X,Y}},south) -> {house, {X,Y-1}};
 follow_direction_from_house({house, {X,Y}},east) -> {house, {X+1,Y}};
 follow_direction_from_house({house, {X,Y}},west) -> {house, {X-1,Y}}.
+
+directions_to_addresses(Directions) ->
+    {_, Reversed_addresses} = lists:foldl(
+            fun(Direction, {{house, {_,_}} = Address, Addresses}) -> 
+                Next_house = directions:follow_direction_from_house(Address, Direction),
+                {Next_house, [Next_house | Addresses]} end, {{house,{0,0}},[{house,{0,0}}]}, Directions),
+    lists:reverse(Reversed_addresses).
+
+number_of_houses_to_visit(Directions) ->
+    Addresses = directions_to_addresses(Directions),
+    Unique_addresses = lists:foldl(
+            fun(K, D) -> dict:append(K,K,D) end,
+            dict:new(),
+            Addresses),
+    dict:size(Unique_addresses).
+
+divide_directions(Directions) ->
+    {Set1, Set2} = divide_directions(Directions, [], [], 0),
+    {lists:reverse(Set1), lists:reverse(Set2)}.
+
+divide_directions([], Set1, Set2, _) -> {Set1, Set2}; 
+divide_directions([H|Tail], Set1, Set2, Index) when Index rem 2 =:= 0 -> 
+    divide_directions(Tail, [H|Set1], Set2, Index + 1);
+divide_directions([H|Tail], Set1, Set2, Index) when Index rem 2 =/= 0 -> 
+    divide_directions(Tail, Set1, [H|Set2], Index + 1).
+    
+    
+
+number_of_houses_to_visit_with_help(Directions) ->
+    {Santa_directions, Robo_santa_directions} = divide_directions(Directions),
+    Santa_addresses = directions_to_addresses(Santa_directions),
+    Robo_santa_addresses = directions_to_addresses(Robo_santa_directions),
+    Santa_unique_addresses = lists:foldl(
+            fun(K, D) -> dict:append(K,K,D) end,
+            dict:new(),
+            Santa_addresses),
+    Combined_unique_addresses = lists:foldl(
+            fun(K, D) -> dict:append(K,K,D) end,
+            Santa_unique_addresses,
+            Robo_santa_addresses),
+    dict:size(Combined_unique_addresses).
+
+houses_to_visit(Filename) -> 
+    Radio_directions = directions:radio_directions_from_file(Filename),
+    Cardinal_directions = directions:cardinal_directions_from_radio_directions(Radio_directions),
+    number_of_houses_to_visit(Cardinal_directions).
+
+houses_to_visit_with_help(Filename) -> 
+    Radio_directions = directions:radio_directions_from_file(Filename),
+    Cardinal_directions = directions:cardinal_directions_from_radio_directions(Radio_directions),
+    number_of_houses_to_visit_with_help(Cardinal_directions).
--- day_3/test/directions_tests.erl
+++ day_3/test/directions_tests.erl
@@ -7,11 +7,41 @@ parse_direction_test() ->
     ?assertEqual(east, directions:parse_direction($>)),
     ?assertEqual(west, directions:parse_direction($<)).
 
+radio_directions_from_file_test() ->
+    ?assertEqual("><<^v^^>", directions:radio_directions_from_file("sample_input.txt")).
+
+cardinal_directions_from_radio_directions_test() ->
+    ?assertEqual([north], directions:cardinal_directions_from_radio_directions("^")),
+    ?assertEqual([north, south], directions:cardinal_directions_from_radio_directions("^v")),
+    ?assertEqual([west, north, east, south], directions:cardinal_directions_from_radio_directions("<^>v")).
+
+divide_dircections_test() ->
+    ?assertEqual({[north], []}, directions:divide_directions([north])),
+    ?assertEqual({[north], [south]}, directions:divide_directions([north, south])),
+    ?assertEqual({[north, east], [south]}, directions:divide_directions([north, south, east])),
+    ?assertEqual({[north, east], [south, west]}, directions:divide_directions([north, south, east, west])).
+
 move_from_house_test() -> 
     ?assertEqual({house, {0,1}}, directions:follow_direction_from_house({house, {0,0}},north)),
     ?assertEqual({house, {0,-1}}, directions:follow_direction_from_house({house, {0,0}},south)),
     ?assertEqual({house, {1,0}}, directions:follow_direction_from_house({house, {0,0}},east)),
     ?assertEqual({house, {-1,0}}, directions:follow_direction_from_house({house, {0,0}},west)).
-
     
+cardinal_directions_to_addresses_test() -> 
+    ?assertEqual([ {house, {0,0}}, {house, {1,0}} ], directions:directions_to_addresses([east])),
+    ?assertEqual([ {house, {0,0}}, {house, {1,0}}, {house,{2,0}} ], directions:directions_to_addresses([east, east])).
 
+number_of_houses_to_visit_test() -> 
+    ?assertEqual(1, directions:number_of_houses_to_visit([])),
+    ?assertEqual(2, directions:number_of_houses_to_visit([east])),
+    ?assertEqual(2, directions:number_of_houses_to_visit([north, south, north, south, north])),
+    ?assertEqual(3, directions:number_of_houses_to_visit([east,south])),
+    ?assertEqual(4, directions:number_of_houses_to_visit([north, east, south, west])),
+    ?assertEqual(3, directions:number_of_houses_to_visit([east,south])).
+
+number_of_houses_to_visit_with_help_test() -> 
+    ?assertEqual(1, directions:number_of_houses_to_visit_with_help([])),
+    ?assertEqual(2, directions:number_of_houses_to_visit_with_help([east])),
+    ?assertEqual(3, directions:number_of_houses_to_visit_with_help([north, south])),
+    ?assertEqual(3, directions:number_of_houses_to_visit_with_help([north, east, south, west])),
+    ?assertEqual(11, directions:number_of_houses_to_visit_with_help([north, south, north, south, north, south, north, south, north, south])).
